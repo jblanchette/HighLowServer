18524945165ef6de74fbe356a64dd458bce9d061
unknown <blanchette@whoop.com>
2015-10-29 23:30:51 -0400

    Basic communication of game list and joining a game.
    Start of structure for SocketManager, and exposed `setup` method.

--- schemas/GameList.js
+++ schemas/GameList.js
@@ -1,6 +1,7 @@
 module.exports = {
 	startGame: "START_GAME",
 	closeGame: "CLOSE_GAME",
+	joinGame: "JOIN_GAME",
 	gameList: "GAME_LIST",
 	kickMember: "KICK_MEMBER"
 };
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var socketio = require("socket.io");
 var io = socketio.listen(server.server);
 
 var SocketManager = require("./lib/SocketManager");
+var GameManager = require("./lib/GameManager");
 
 io.sockets.on('connection', function (socket) {
 	SocketManager.onConnect(socket);
@@ -11,4 +12,10 @@ io.sockets.on('connection', function (socket) {
 
 server.listen(8080, function () {
 	console.log("going");
+
+	SocketManager.setup(GameManager);
+
+	for (var i = 0; i < 5; i++) {
+		GameManager.createGame();
+	}
 });
