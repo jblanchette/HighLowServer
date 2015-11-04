4227a39df815d8bf24f891d3847c44615e7a4c93
jblanchette <jeff.blanchette5@gmail.com>
2015-11-03 23:36:55 -0500

    Finish up the functional parts of the chat manager. Untested.

--- schemas/Chat.js
+++ schemas/Chat.js
@@ -1,7 +1,8 @@
 module.exports = {
   joinRoom: "JOIN_ROOM",
   leaveRoom: "LEAVE_ROOM",
-  roomMessage: "ROOM_MSG"
-
+  roomMessage: "ROOM_MSG",
+  memberJoined: "MEMBER_JOINED",
+  memberLeft: "MEMBER_LEFT"
   // todo: private rooms, admin stuff, ect
 };
