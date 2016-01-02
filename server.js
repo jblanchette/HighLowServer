9b9e310db15feed02daa6f4a47fc5419243c4c3b
jblanchette <jeff.blanchette5@gmail.com>
2016-01-02 12:18:52 -0500

    Add more support for chat management.  Start basics of global chat room handling, and inital chat server connecting.

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var io = socketio.listen(server.server);
 
 var SocketManager = require("./lib/SocketManager");
 var GameManager = require("./lib/GameManager");
+var ChatManager = require("./lib/ChatManager");
 
 SocketManager.setServerInstance(io);
 
@@ -14,10 +15,14 @@ io.sockets.on('connection', function (socket) {
 
 server.listen(8080, function () {
 	console.log("Starting server on port 8080");
+  console.log("=======================================================\n");
 	SocketManager.createNamespace(GameManager.getNamespace());
-
-	console.log("Making games...");
+  SocketManager.createNamespace(ChatManager.getNamespace());
+  	
+  console.log("Making games...");
 	for (var i = 0; i < 5; i++) {
 		GameManager.createGame();
 	}
+
+  ChatManager.createGlobalRoom();
 });
