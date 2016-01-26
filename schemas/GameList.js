bfd3b76ba5a615bce7a9fe2b733b439a5fe8add4
jblanchette <jeff.blanchette5@gmail.com>
2016-01-25 22:13:37 -0500

    Add more to the game manager support for joining and leaving games.
    Fix some socket interactions.
    
    now emits an event to the game list whenever anyone leaves,
    marking the specific room they left.
    
    probably will eventually be a lot of overhead to do this,
    and can switch clients over to a 'refresh' poll.

--- schemas/GameList.js
+++ schemas/GameList.js
@@ -2,6 +2,9 @@ module.exports = {
 	startGame: "START_GAME",
 	closeGame: "CLOSE_GAME",
 	joinGame: "JOIN_GAME",
+  leftGame: "LEFT_GAME",
+  userLeft: "USER_LEFT",
+  userJoined: "USER_JOINED",
 	gameList: "GAME_LIST",
 	kickMember: "KICK_MEMBER"
 };
