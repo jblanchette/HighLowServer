b53cfb5d75d7476c2dd8dcb1badab6cda9d891f0
jblanchette <jeff.blanchette5@gmail.com>
2016-03-06 21:04:59 -0500

    Add new format for chat responses.  Fix some issues with leaving all chat rooms not sending socket messages.

--- schemas/Chat.js
+++ schemas/Chat.js
@@ -2,6 +2,7 @@ module.exports = {
   joinRoom: "JOIN_ROOM",
   joinGlobalRoom: "JOIN_GLOBAL",
   leaveRoom: "LEAVE_ROOM",
+  leftRoom: "LEFT_ROOM",
   roomMessage: "ROOM_MSG",
   userJoined: "USER_JOINED",
   userLeft: "USER_LEFT"
